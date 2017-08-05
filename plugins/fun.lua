adfc1ebc4c51ed12d17b22e5146969226c3a777e
ESMAILESMAIL <sokot121@chmail.ir>
2017-08-05 19:21:17 +0430

    Update fun.lua

--- plugins/fun.lua
+++ plugins/fun.lua
@@ -1,6 +1,6 @@
 
---Begin Fun.lua By @BeyondTeam
---Special Thx To @To0fan
+--Begin Fun.lua By @IranDev_Team
+--Special Thx To @EsMailMa
 --------------------------------
 
 local function run_bash(str)
@@ -235,7 +235,7 @@ local Clang = redis:get(Chash)
       return nil
       else
   local url = "http://tts.baidu.com/text2audio?lan=en&ie=UTF-8&text="..textc
-  local file = download_to_file(url,'BD-Reborn.mp3')
+  local file = download_to_file(url,'DangerBot.mp3')
  				tdcli.sendDocument(msg.to.id, 0, 0, 1, nil, file, msg_caption, dl_cb, nil)
    end
 end
@@ -314,7 +314,7 @@ local hash = "gp_lang:"..msg.to.id
 local lang = redis:get(hash)
 if not lang then
 helpfun_en = [[
-_Beyond Reborn Fun Help Commands:_
+_Danger Bot Fun Help Commands:_
 
 *!time*
 _Get time in a sticker_
@@ -357,7 +357,7 @@ _You can use_ *[!/#]* _at the beginning of commands._
 else
 
 helpfun_en = [[
-_راهنمای فان ربات بیوند:_
+_راهنمای فان ربات دانجر:_
 
 *!time*
 _دریافت ساعت به صورت استیکر_
@@ -406,7 +406,7 @@ local hash = "gp_lang:"..msg.to.id
 local lang = redis:get(hash)
 if not lang then
 helpfun_fa = [[
-_Beyond Reborn Fun Help Commands:_
+_Danger Bot Fun Help Commands:_
 
 *ساعت*
 _Get time in a sticker_
@@ -447,7 +447,7 @@ _Get weather_
 else
 
 helpfun_fa = [[
-_راهنمای فان ربات بیوند:_
+_راهنمای فان ربات دانجر:_
 
 *ساعت*
 _دریافت ساعت به صورت استیکر_
@@ -523,4 +523,4 @@ return {
 	run = run,
 	}
 
---#by @BeyondTeam :)
+--#by @IranDev_Team :)
